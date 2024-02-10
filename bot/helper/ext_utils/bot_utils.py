d880816ba6ed67082c912d4cbaed1b76b49351e3
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2024-02-10 17:22:43 -0500

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -38,7 +38,7 @@ SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 STATUS_START = 0
 PAGES = 1
 PAGE_NO = 1
-STATUS_LIMIT = 4
+STATUS_LIMIT = 1
 
 class MirrorStatus:
     STATUS_UPLOADING = "Uploading"
